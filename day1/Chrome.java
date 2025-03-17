1b092cd61a6a674ee31b3438e4989f6d1ff075c1
Poojashree281 <poojapunith21@gmail.com>
2025-03-17 22:12:13 +0530

    Add files via upload
    day1

---
+++ day1/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version=91.0f;
+		String developer="Google";
+		boolean isBeta=false;
+		short releaseYear=2008;
+		char shortcutKey='C';
+		System.out.println(version);
+		System.out.println(isBeta);
+		System.out.println(shortcutKey);
+		System.out.println(developer);
+		System.out.println(releaseYear);
+				
+	}
+
+}
